6b9c4fcadbe33f7cd196490b4e0ece6e89138039
vaallakhverdyants <vaallakhverdyants@avito.ru>
2024-02-01 20:04:19 +0300

    remove unused layers add CLI for application usage

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
+import argparse
+import json
 from dependency import get_journey_service
 from core.models.boarding_card import BoardingCard
-def main(boarding_card_data):
+
+
+def main(boarding_cards):
     """
     Takes a list of boarding card information and prints the journey description.
 
-    :param boarding_card_data: A list of dictionaries, each representing a boarding card's information.
-    :type boarding_card_data: list of dict
+    :param boarding_cards: A list of dictionaries, each representing a boarding card's information.
+    :type boarding_cards: list of dict
     """
     service = get_journey_service()
 
-    for card_info in boarding_card_data:
-        card = BoardingCard(
-            transport=card_info['transport'],
-            departure=card_info['departure'],
-            destination=card_info['destination'],
-            seat=card_info['seat']
-        )
+    for card_info in boarding_cards:
+        card = BoardingCard(**card_info)
         service.add_boarding_card(card)
 
     print(service.get_journey())
 
 
-# Example usage:
 if __name__ == "__main__":
-    boarding_card_data = [
-        {"transport": "Train 78A", "departure": "Madrid", "destination": "Barcelona",
-         "seat": "45B"},
-        {"transport": "Airport Bus", "departure": "Barcelona", "destination": "Gerona Airport",
-         "seat": "No seat assignment"},
-        {"transport": "Flight SK455", "departure": "Gerona Airport", "destination": "Stockholm",
-         "seat": "3A", "extra_info": {"Gate": "45B", "Baggage drop": "ticket counter 344"}},
-        {"transport": "Flight SK22", "departure": "Stockholm", "destination": "New York JFK",
-         "seat": "7B", "extra_info": {"Gate": "22"}}
-    ]
-
+    parser = argparse.ArgumentParser(description="Sort and display a journey based on boarding cards")
+    parser.add_argument('--boarding-cards', type=str, help='JSON list of boarding card data as strings')
+    args = parser.parse_args()
+    boarding_cards_arg = args.boarding_cards
+    boarding_card_data = json.loads(boarding_cards_arg)
     main(boarding_card_data)
